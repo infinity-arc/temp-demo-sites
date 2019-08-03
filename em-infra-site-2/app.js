54a0355affc1d379f61ea2df307a31f438f95bf7
Cliff Crerar <cliff.crerar@gmail.com>
2019-08-03 22:43:41 +0200

    Uploaded a lot of stuff

--- em-infra-site-2/app.js
+++ em-infra-site-2/app.js
@@ -2,20 +2,45 @@ const { send } = require('micro')
 const get = require('micro-get');
 const fs = require('fs');
 const path = require('path');
+const cors = require('micro-cors');
+const md = require('markdown-it')();
+var markdownItAttrs = require('markdown-it-attrs');
 
-module.exports = get(async(req, res) => {
+md.use(markdownItAttrs, {
+    // optional, these are default options
+    leftDelimiter: '{',
+    rightDelimiter: '}',
+    allowedAttributes: [] // empty array = all attributes are allowed
+});
+
+const test = md.render(fs.readFileSync(path.join(__dirname, 'SolutionDesc.md'), 'utf8'));
+console.log('test: ', test);
+
+const handleGet = get(async(req, res) => {
     //console.log('req: ', req);
     //return send(res, 200, `It's a GET request!`);
     //res.setHeaders('Content-type', 'text/html')
-    return getResponse(req.url, res);
-})
+    return getResponse(req, res);
+});
 
-function getResponse(query, res) {
-    console.log('query: ', query);
-    switch (query) {
-        case '/':
-            return send(res, 200, fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8'));
-        default:
-            return send(res, 400, 'Bad request');
+function getResponse(req, res) {
+    console.log('query: ', req.url);
+    try {
+        switch (req.url) {
+            case '/':
+                return send(res, 200, fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8'));
+            case '/mdsheet':
+                return send(res, 200, renderMarkdown('SolutionDesc.md'));
+            case '/favicon.ico':
+                return send(res, 100, 'Fuck the favicon!');
+            default:
+                return send(res, 400, 'Bad request');
+        }
+    } catch (err) {
+        return send(res, 500, `CAUGHT Server error: ${err}`);
     }
 }
+
+const renderMarkdown = (file) => md.render(fs.readFileSync(path.join(__dirname, file), 'utf8'));
+
+module.exports = handleGet;
